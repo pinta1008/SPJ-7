f27a3eb7dd10cd78ef844d5ef44cc13020385bb8
pinta1008 <57998225+pinta1008@users.noreply.github.com>
2022-05-28 17:14:54 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Ucitaj from './komponente/Ucitaj';
 import Dodaj from './komponente/Dodaj';
 import Nav from './komponente/Navigacija';
+import Edit from './komponente/Edit';
+import FilterTable from './komponente/FilterTable';
 
 
 
@@ -11,8 +13,10 @@ function App() {
    <BrowserRouter>
    <Nav />
       <Routes>
-        <Route path='Ucitaj' element={<Ucitaj/>} />
-        <Route path='Dodaj'element={<Dodaj/>} />
+        <Route path='/Ucitaj' element={<Ucitaj/>} />
+        <Route path='/Dodaj'element={<Dodaj/>} />
+        <Route path='/Ucitaj/Edit/:EditID' element={<Edit />} />
+        <Route path='/Pretrazivanje' element={<FilterTable />} />
       </Routes>
    </BrowserRouter>
   );
